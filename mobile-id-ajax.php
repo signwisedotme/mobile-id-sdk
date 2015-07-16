8b4d3713763b9ab1285bc8e77f55627bbf5025fb
Kaspar Aasamets <kaspar.aasamets@cbts.eu>
2015-07-16 16:35:36 +0300

    Initial commit

---
+++ mobile-id-ajax.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * An example SignWise Services Mobile ID endpoint accessed with AJAX calls.
+ * The purpose of this script is to initiate Mobile ID authentication and signing requests
+ * and to check if response has been received for Mobile ID request.
+ *
+ * Mobile sessions get stored in text file only for sake of simplicity.
+ * You should use a session database instead.
+ *
+ * See mobile-id-callback.php code to see how Mobile ID session statuses get stored in file.
+ *
+ * SignWise Services PHP SDK is a required dependency.
+ */
+
+session_start();
+
+require_once 'class.SignWise.php';
+
+$swConf = array(
+  'server' => 'https://dtm-test.signwise.me/',
+  'certificate' => 'cert/cert.crt',
+  'privateKey' => 'cert/private.key',
+  'defaultFileProxyUrl' => 'http://www.example.com/my-files/',
+  'defaultContainerType' => 'bdoc',
+);
+$sw = new SignWise($swConf);
+
+$data = json_decode(file_get_contents('php://input'));
+
+if (isset($_GET['auth'])) {
+  // Start Mobile ID authentication
+  $userInfo = array("language" => $data->lang, "ssn" => $data->ssn, "msisdn" => $data->msisdn);
+  $mobileSession = uniqid('', true);
+  $_SESSION['mobile_id_auth_session'] = $mobileSession;
+  $result = $sw->mobileAuthentication("http://www.example.com/callback.php?mobileSession=" . $mobileSession, $userInfo);
+  if ($result->verificationCode) {
+    echo json_encode(array('verificationCode' => $result->verificationCode));
+  } else {
+    echo json_encode($result);
+  }
+} elseif (isset($_GET['auth_result'])) {
+  // Get Mobile ID authentication result
+  echo json_encode(getMobileIdSessionResult('authenticate', $_SESSION['mobile_id_auth_session']));
+} elseif (isset($_GET['sign'])) {
+  // Start Mobile ID signing
+  $userInfo = array("language" => $data->lang, "ssn" => $data->ssn, "msisdn" => $data->msisdn);
+  $mobileSession = uniqid('', true);
+  $_SESSION['mobile_id_sign_session'] = $mobileSession;
+  $result = $sw->mobileSigning($data->container, $data->container . ".tmp", "http://www.example.com/callback.php?mobileSession=" . $mobileSession, $userInfo);
+  if ($result->verificationCode) {
+    echo json_encode(array('verificationCode' => $result->verificationCode));
+  } else {
+    echo json_encode($result);
+  }
+} elseif (isset($_GET['sign_result'])) {
+  // Get Mobile ID signing result
+  echo json_encode(getMobileIdSessionResult('sign', $_SESSION['mobile_id_sign_session']));
+}
+
+function getMobileIdSessionResult($operation, $sessionId) {
+  if (($handle = fopen("out.txt", "r")) !== FALSE) {
+    while (false !== ($row = fgetcsv($handle, 1000, ";"))) {
+      @list($rOperation, $rSessionId, $rStatus, $rCountry, $rSsn, $rFirstName, $rLastName) = $row;
+      if ($rOperation === $operation && $rSessionId === $sessionId) {
+        if ($operation === "authenticate" && $rStatus === "OK") {
+          $result = array(
+            "status" => $rStatus,
+            "country" => $rCountry,
+            "personalCode" => $rSsn,
+            "firstName" => $rFirstName,
+            "lastName" => $rLastName,
+          );
+        } else {
+          $result = array(
+            "status" => $rStatus,
+          );
+        }
+        break;
+      }
+    }
+    fclose($handle);
+  }
+  if (!isset($result)) {
+    $result = array("status" => "PENDING");
+  }
+  return $result;
+}
