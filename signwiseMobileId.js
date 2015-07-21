0bf8346f748e1493114d85add5a36346523913ed
Kaspar Aasamets <kaspar.aasamets@cbts.eu>
2015-07-21 12:41:21 +0300

    Apache 2.0 license

--- signwiseMobileId.js
+++ signwiseMobileId.js
@@ -1,11 +1,28 @@
 "use strict";
 
+/*
+ Copyright 2015 SignWise Corporation Ltd.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 /* Version 1.0.0 */
 
 /**
  * SignWise Mobile ID SDK
  *
  * Possible error codes:
+ *   TIMEOUT
  *   MOBILE_PHONE_PERSONAL_CODE_MISMATCH
  *   INVALID_MID_PARAMETERS
  *   MID_NOT_ACTIVATED
@@ -75,6 +92,7 @@
       if (--countdown <= 0) {
         timedOut = true;
         countdownCb(false);
+        cb({status: "TIMEOUT"});
         clearCountdown();
       } else {
         countdownCb(countdown);
